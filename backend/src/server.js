63b29f0e349fa5f6cb7c63ca223a945759fee309
Sadeepa Dinakara <sadeepa14dinakara@gmail.com>
2026-07-11 02:08:41 +0530

    Change module type to commonjs and update User model to use CommonJS syntax

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const sequelize = require("./config/database");
+const sequelize = require("../config/database.cjs");
 
 const userRoutes = require("./routes/UserRoute");
 
@@ -10,7 +9,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(cookieParser());
 app.use(express.json());
 
 app.get("/", (req, res) => {
