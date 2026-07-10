ec7f46af9e56aeeee8454cdebcfb0607d27da694
Sadeepa Dinakara <sadeepa14dinakara@gmail.com>
2026-07-11 00:58:07 +0530

    Initialize server with Express, CORS, and cookie parsing; set up user routes and database connection

---
+++ backend/src/server.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const sequelize = require("./config/database");
+
+const userRoutes = require("./routes/UserRoute");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "FLCMS API is running",
+  });
+});
+app.use("/api/users", userRoutes);
+
+const startServer = async () => {
+  try {
+    console.log("Connecting to the database...");
+    await sequelize.authenticate();
+    console.log("Database connection established successfully.✅");
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("\n❌ Server startup failed:");
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
