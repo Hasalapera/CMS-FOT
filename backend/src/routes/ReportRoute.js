4ac1eed9bcd0fbef7bb2d9ba89075b8573be6971
Hasalapera <hasalashe777@gmail.com>
2026-07-16 23:21:59 +0530

    verify tocken problem

--- backend/src/routes/ReportRoute.js
+++ backend/src/routes/ReportRoute.js
@@ -1,19 +1,21 @@
+const reportController = require("../controllers/ReportController");
+const verifyToken = require('../middlewares/Authmiddleware.js');
 const express = require("express");
 const router = express.Router();
-const reportController = require("../controllers/ReportController");
 
 // chemical wise report
-router.get("/chemicals/:chemicalCode", reportController.getChemicalReport);
+router.get("/chemicals/:chemicalCode",verifyToken, reportController.getChemicalReport);
 router.get(
   "/chemicals/:chemicalCode/download",
+  verifyToken,
   reportController.downloadChemicalReport,
 );
 
 // usage report
-router.get("/usage", reportController.getUsageReport);
-router.get("/usage/download", reportController.downloadUsageReport);
+router.get("/usage", verifyToken, reportController.getUsageReport);
+router.get("/usage/download",verifyToken, reportController.downloadUsageReport);
 
 // Full Inventory Status Report
-router.get("/inventory/download", reportController.downloadFullInventoryReport);
+router.get("/inventory/download", verifyToken, reportController.downloadFullInventoryReport);
 
 module.exports = router;
